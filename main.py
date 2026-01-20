8a22e2e1e38e5500e3437b6dc16171e363bfeb9c
mzbotz26 <mrsam2395@gmail.com>
2026-01-20 20:38:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
 import asyncio
 from bot import Bot
-from config import LOGGER
-
 
 async def main():
     bot = Bot()
     await bot.start()
-    LOGGER(__name__).info("Bot started successfully!")
-    await asyncio.Event().wait()  # bot ko hamesha running rakhe
-
+    await asyncio.Event().wait()   # bot alive rahe
 
 if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except KeyboardInterrupt:
-        LOGGER(__name__).info("Bot stopped by user")
-    except Exception as e:
-        LOGGER(__name__).error(f"Fatal error: {e}")
+    asyncio.run(main())
